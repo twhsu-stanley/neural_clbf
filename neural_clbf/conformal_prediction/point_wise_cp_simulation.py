f421d37e67300bd2e5f4c3440fce128e77e11630
twhsu-stanley <twhsu@umich.edu>
2024-11-08 01:27:41 -0600

    update cp quantile code

--- neural_clbf/conformal_prediction/point_wise_cp_simulation.py
+++ neural_clbf/conformal_prediction/point_wise_cp_simulation.py
@@ -3,57 +3,72 @@ from neural_clbf.controllers import NeuralCLBFController
 import numpy as np
 import cvxpy as cp
 from cvxpylayers.torch import CvxpyLayer
+import pickle
 
+# TODO: Write a function that creates a CvxpyLayer solver
+#       Write a function that simulates trajectories using clf_qp_cp
 
-# Load the learned CLF
-log_file = "./logs/inverted_pendulum_sindy/commit_c046f61/version_2/checkpoints/epoch=24-step=3524.ckpt"
-neural_controller = NeuralCLBFController.load_from_checkpoint(log_file)
+def create_clf_qp_cp_cvxpylayers_solver(neural_controller):
+    """Create a solver for the CLF-QP-CP problem using CvxpyLayer
+    """
 
-# Define variables
-u = cp.Variable(neural_controller.dynamics_model.n_controls)
-clf_relaxations = cp.Variable(1, nonneg=True)
+    # Define variables
+    u = cp.Variable(neural_controller.dynamics_model.n_controls)
+    clf_relaxations = cp.Variable(1, nonneg=True)
 
-# Define the parameters that will be supplied at solve-time: the value
-# of the Lyapunov function, its Lie derivatives, the relaxation penalty, and
-# the reference control input
-Lf_V_params = cp.Parameter(1)
-Lg_V_params = cp.Parameter(neural_controller.dynamics_model.n_controls)
-V_param = cp.Parameter(1, nonneg=True)
-cp_quantile_param = cp.Parameter(1, nonneg=True)
-u_ref_param = cp.Parameter(neural_controller.dynamics_model.n_controls)
-clf_relaxation_penalty_param = cp.Parameter(1, nonneg=True)
+    # Define the parameters that will be supplied at solve-time: the value
+    # of the Lyapunov function, its Lie derivatives, the relaxation penalty, and
+    # the reference control input
+    Lf_V_params = cp.Parameter(1)
+    Lg_V_params = cp.Parameter(neural_controller.dynamics_model.n_controls)
+    V_param = cp.Parameter(1, nonneg=True)
+    cp_quantile_param = cp.Parameter(1, nonneg=True)
+    u_ref_param = cp.Parameter(neural_controller.dynamics_model.n_controls)
+    clf_relaxation_penalty_param = cp.Parameter(1, nonneg=True)
 
-constraints = []
-# CLF decrease constraint (with relaxation)
-constraints.append(
-    Lf_V_params
-    + Lg_V_params @ u
-    + neural_controller.clf_lambda * V_param
-    + cp_quantile_param
-    - clf_relaxations
-    <= 0
-)
+    constraints = []
+    # CLF decrease constraint (with relaxation)
+    constraints.append(
+        Lf_V_params
+        + Lg_V_params @ u
+        + neural_controller.clf_lambda * V_param
+        + cp_quantile_param
+        - clf_relaxations
+        <= 0
+    )
 
-# Control limit constraints
-upper_lim, lower_lim = neural_controller.dynamics_model.control_limits
-for control_idx in range(neural_controller.dynamics_model.n_controls):
-    constraints.append(u[control_idx] >= lower_lim[control_idx])
-    constraints.append(u[control_idx] <= upper_lim[control_idx])
+    # Control limit constraints
+    upper_lim, lower_lim = neural_controller.dynamics_model.control_limits
+    for control_idx in range(neural_controller.dynamics_model.n_controls):
+        constraints.append(u[control_idx] >= lower_lim[control_idx])
+        constraints.append(u[control_idx] <= upper_lim[control_idx])
 
-# Define the objective
-objective_expression = cp.sum_squares(u - u_ref_param)
-objective_expression += cp.multiply(clf_relaxation_penalty_param, clf_relaxations)
-objective = cp.Minimize(objective_expression)
+    # Define the objective
+    objective_expression = cp.sum_squares(u - u_ref_param)
+    objective_expression += cp.multiply(clf_relaxation_penalty_param, clf_relaxations)
+    objective = cp.Minimize(objective_expression)
 
-# Create the optimization problem
-problem = cp.Problem(objective, constraints)
-assert problem.is_dpp()
-variables = [u] + [clf_relaxations]
-parameters = [Lf_V_params] + [Lg_V_params]
-parameters += [V_param, cp_quantile_param, u_ref_param, clf_relaxation_penalty_param]
-differentiable_qp_cp_solver = CvxpyLayer(
-    problem, variables=variables, parameters=parameters
-)
+    # Create the optimization problem
+    problem = cp.Problem(objective, constraints)
+    assert problem.is_dpp()
+    variables = [u] + [clf_relaxations]
+    parameters = [Lf_V_params] + [Lg_V_params]
+    parameters += [V_param, cp_quantile_param, u_ref_param, clf_relaxation_penalty_param]
+    clf_qp_cp_solver = CvxpyLayer(
+        problem, variables=variables, parameters=parameters
+    )
 
+    return clf_qp_cp_solver
 
-neural_controller.u_CLF_QP_CP(torch.zeros(2, 2), differentiable_qp_cp_solver, torch.zeros(1))
+if __name__ == "__main__":
+    # Load the learned CLF
+    log_file = "./logs/inverted_pendulum_sindy/commit_c046f61/version_2/checkpoints/epoch=24-step=3524.ckpt"
+    neural_controller = NeuralCLBFController.load_from_checkpoint(log_file)
+
+    # Load the saved CP quantile
+    with open('./neural_clbf/conformal_prediction/quantiles/inverted_pendulum_sindy/' + 'point_wise_cp_quantile', 'rb') as file:
+        point_wise_cp_quantile = pickle.load(file)
+
+    clf_qp_cp_solver = create_clf_qp_cp_cvxpylayers_solver(neural_controller)
+
+    u_test = neural_controller.u_CLF_QP_CP(torch.zeros(2, 2), clf_qp_cp_solver, point_wise_cp_quantile)
