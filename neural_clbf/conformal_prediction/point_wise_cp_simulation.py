de8ed5fdfb571ada6720a8c8ade58ddd1852f1f6
twhsu-stanley <twhsu@umich.edu>
2024-11-08 18:25:55 -0600

    preliminary sim code runs

--- neural_clbf/conformal_prediction/point_wise_cp_simulation.py
+++ neural_clbf/conformal_prediction/point_wise_cp_simulation.py
@@ -4,6 +4,10 @@ import numpy as np
 import cvxpy as cp
 from cvxpylayers.torch import CvxpyLayer
 import pickle
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')
 
 # TODO: Write a function that creates a CvxpyLayer solver
 #       Write a function that simulates trajectories using clf_qp_cp
@@ -53,12 +57,57 @@ def create_clf_qp_cp_cvxpylayers_solver(neural_controller):
     assert problem.is_dpp()
     variables = [u] + [clf_relaxations]
     parameters = [Lf_V_params] + [Lg_V_params]
-    parameters += [V_param, cp_quantile_param, u_ref_param, clf_relaxation_penalty_param]
+    parameters += [V_param, cp_quantile_param, u_ref_param, clf_relaxation_penalty_param]#
     clf_qp_cp_solver = CvxpyLayer(
         problem, variables=variables, parameters=parameters
     )
 
     return clf_qp_cp_solver
+
+def clf_qp_cp_simulation(neural_controller, clf_qp_cp_solver, point_wise_cp_quantile, start_x, T):
+
+    # Compute the number of simulations to run
+    n_sims = start_x.shape[0]
+    
+    # Generate a tensor of start states
+    n_dims = neural_controller.dynamics_model.n_dims
+    n_controls = neural_controller.dynamics_model.n_controls
+    x_sim_start = start_x
+
+    # Make sure everything's on the right device
+    device = "cpu"
+    if hasattr(neural_controller, "device"):
+        device = neural_controller.device  # type: ignore
+    x_current = x_sim_start.to(device)
+
+    # Simulate
+    delta_t = neural_controller.dynamics_model.dt
+    num_timesteps = int(T // delta_t)
+    u_current = torch.zeros(x_sim_start.shape[0], n_controls, device=device)
+    
+    x_history = np.zeros((n_sims, n_dims, num_timesteps))
+
+    for t in range(num_timesteps):
+        # Compute control input by solving the CLF-QP-CP problem
+        u_current, _ = neural_controller.u_CLF_QP_CP(x_current, clf_qp_cp_solver, point_wise_cp_quantile)
+        #u_current = neural_controller.u(x_current)
+
+        # Simulate forward using the dynamics
+        for i in range(n_sims):
+            # TODO: use the ground truth model
+            xdot = neural_controller.dynamics_model.closed_loop_dynamics(
+                x_current[i, :].unsqueeze(0),
+                u_current[i, :].unsqueeze(0)
+            )
+            x_current[i, :] = x_current[i, :] + delta_t * xdot.squeeze()
+
+        x_history[:,:,t] = x_current.cpu().detach().numpy()
+
+    # Plot
+    plt.figure("State 2-norm")
+    for i in range(n_sims):
+        plt.plot(np.arange(num_timesteps) * delta_t, np.linalg.norm(x_history[i,:,:].squeeze().T, axis=1))
+    plt.show()
 
 if __name__ == "__main__":
     # Load the learned CLF
@@ -69,6 +118,21 @@ if __name__ == "__main__":
     with open('./neural_clbf/conformal_prediction/quantiles/inverted_pendulum_sindy/' + 'point_wise_cp_quantile', 'rb') as file:
         point_wise_cp_quantile = pickle.load(file)
 
+    # Create a QP solver
     clf_qp_cp_solver = create_clf_qp_cp_cvxpylayers_solver(neural_controller)
+    
+    u1, _ = neural_controller.u_CLF_QP_CP(torch.tensor([[0.0, 1.5]]), clf_qp_cp_solver, 0)
+    u2 = neural_controller.u(torch.tensor([[0.0, 1.5]]))
+    assert u1 == u2
 
-    u_test = neural_controller.u_CLF_QP_CP(torch.zeros(2, 2), clf_qp_cp_solver, point_wise_cp_quantile)
+    # Set up initial conditions for the sim
+    start_x = torch.tensor(
+        [
+            [1.5, 1.5],
+            #[0.9, 1.5],
+            [0.3, 1.5],
+        ]
+    )
+
+    # Run the sim
+    clf_qp_cp_simulation(neural_controller, clf_qp_cp_solver, point_wise_cp_quantile, start_x, T = 3)
