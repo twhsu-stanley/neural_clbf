036e8afa4a5e63ac0b74b46c3b22771335c0a68c
twhsu-stanley <twhsu@umich.edu>
2025-02-27 10:43:45 -0600

    try scale_parameter=10

--- neural_clbf/training/train_dubins_car_sindy.py
+++ neural_clbf/training/train_dubins_car_sindy.py
@@ -105,7 +105,7 @@ def main(args):
         cbf_lambda=0.1,
         controller_period=controller_period,
         cbf_relaxation_penalty=1e4,
-        scale_parameter=1.0,
+        scale_parameter=10.0,
         primal_learning_rate=5e-3,
         learn_shape_epochs=80,
         cp_learning = True,
