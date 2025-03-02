beeec1a60f45de9eb7aaa664b593371ae37e6733
twhsu-stanley <twhsu@umich.edu>
2025-03-01 20:50:40 -0600

    update obstacle = rec+circle

--- neural_clbf/systems/dubins_car_sindy.py
+++ neural_clbf/systems/dubins_car_sindy.py
@@ -153,11 +153,11 @@ class DubinsCarSINDy(ControlAffineSystem):
         safe_mask = torch.ones_like(x[:, 0], dtype=torch.bool)
 
         # circular obstacle
-        #d = torch.pow(x[:,DubinsCarSINDy.X] - 0.0, 2) + torch.pow(x[:,DubinsCarSINDy.Y] - 0.0, 2) - 4
-        #safe_mask.logical_and_(d >= 3.0)
+        d = torch.pow(x[:,DubinsCarSINDy.X] - 0.0, 2) + torch.pow(x[:,DubinsCarSINDy.Y] - 0.0, 2) - 4
+        safe_mask.logical_and_(d >= 3.0)
 
         # rectangular obstacle
-        safe_mask.logical_and_(torch.logical_or(torch.abs(x[:,DubinsCarSINDy.X]) > 3, x[:,DubinsCarSINDy.Y] < -3))
+        safe_mask.logical_and_(torch.logical_or(torch.abs(x[:,DubinsCarSINDy.X]) > 2.5, x[:,DubinsCarSINDy.Y] < -1))
         #safe_mask.logical_and_(x[:,DubinsCarSINDy.Y] <= 3)
 
         # theta constraint
@@ -175,11 +175,11 @@ class DubinsCarSINDy(ControlAffineSystem):
         unsafe_mask = torch.zeros_like(x[:, 0], dtype=torch.bool)
 
         # circular obstacle
-        #d = torch.pow(x[:,DubinsCarSINDy.X] - 0.0, 2) + torch.pow(x[:,DubinsCarSINDy.Y] - 0.0, 2) - 4
-        #unsafe_mask.logical_or_(d <= 0)
+        d = torch.pow(x[:,DubinsCarSINDy.X] - 0.0, 2) + torch.pow(x[:,DubinsCarSINDy.Y] - 0.0, 2) - 4
+        unsafe_mask.logical_or_(d <= 0)
 
         # rectangular obstacle
-        unsafe_mask.logical_or_(torch.logical_and(torch.abs(x[:,DubinsCarSINDy.X]) < 2, x[:,DubinsCarSINDy.Y] > -2))
+        unsafe_mask.logical_or_(torch.logical_and(torch.abs(x[:,DubinsCarSINDy.X]) < 2, x[:,DubinsCarSINDy.Y] > 0))
         #unsafe_mask.logical_or_(x[:,DubinsCarSINDy.Y] > 4)
 
         # theta constraint
