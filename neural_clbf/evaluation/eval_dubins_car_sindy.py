7779c4c60d68d6e5d40387c1764129ae1986f232
twhsu-stanley <twhsu@umich.edu>
2025-03-01 22:37:29 -0600

    update dubins eval

--- neural_clbf/evaluation/eval_dubins_car_sindy.py
+++ neural_clbf/evaluation/eval_dubins_car_sindy.py
@@ -14,20 +14,20 @@ matplotlib.use('TkAgg')
 
 def plot_dubins_cbf():
     # Load the checkpoint file. This should include the experiment suite used during training
-    log_file = "logs/dubins_car_sindy/commit_52172ea/version_0/checkpoints/epoch=222-step=15832.ckpt"
-    #log_file = "logs/dubins_car_sindy/commit_52172ea/version_0/checkpoints/epoch=259-step=18459.ckpt"
     log_file = "logs/dubins_car_sindy/commit_52172ea/version_0/checkpoints/epoch=300-step=21370.ckpt"
+    log_file = "logs/dubins_car_sindy/commit_c879c51/version_0/checkpoints/epoch=100-step=7170.ckpt"
     
     neural_controller = NeuralCBFController.load_from_checkpoint(log_file)
 
     # Tweak parameters
-    #neural_controller.cbf_relaxation_penalty = 0
-    neural_controller.clf_lambda = 0.5
-    #neural_controller.controller_period = 0.005
+    #neural_controller.cbf_relaxation_penalty = 1e9
+    neural_controller.clf_lambda = 0.1
+    #neural_controller.controller_period = 0.001
+    #neural_controller.disable_gurobi = True
 
     V_contour_experiment_1 = CLFContourExperiment(
         "V_Contour",
-        domain = [(-5.0, 5.0), (-5.0, 5.0)],
+        domain = [(-6.0, 6.0), (-6.0, 6.0)],
         n_grid = 40,
         x_axis_index = 0,
         y_axis_index = 1,
@@ -36,7 +36,7 @@ def plot_dubins_cbf():
     )
     V_contour_experiment_2 = CLFContourExperiment(
         "V_Contour",
-        domain = [(-5.0, 5.0), (-180/180*np.pi, 180/180*np.pi)],
+        domain = [(-6.0, 6.0), (-100/180*np.pi, 100/180*np.pi)],
         n_grid = 40,
         x_axis_index = 0,
         y_axis_index = 2,
@@ -45,27 +45,27 @@ def plot_dubins_cbf():
     )
     V_contour_experiment_3 = CLFContourExperiment(
         "V_Contour",
-        domain = [(-5.0, 5.0), (-180/180*np.pi, 180/180*np.pi)],
+        domain = [(-6.0, 6.0), (-100/180*np.pi, 100/180*np.pi)],
         n_grid = 40,
         x_axis_index = 1,
         y_axis_index = 2,
         x_axis_label = "p_y",
         y_axis_label = "theta",
     )
-    neural_controller.experiment_suite = ExperimentSuite([V_contour_experiment_1, V_contour_experiment_2, V_contour_experiment_3])
+    neural_controller.experiment_suite = ExperimentSuite([V_contour_experiment_1])
     #neural_controller.experiment_suite.run_all_and_plot(neural_controller, display_plots = True)
 
     # Set up initial conditions for the sim
     N = 10 # number of trajectories
     start_x = torch.hstack((
-        torch.rand(N,1) * 0 - 3.5,
-        torch.rand(N,1) * 0.2 - 0.1,
+        torch.rand(N,1) * 1 - 5,
+        torch.rand(N,1) * 1 - 0.5,
         torch.rand(N,1) * 0/180*np.pi - 0/180*np.pi
     ))
     #start_x = start_x[torch.pow(start_x[:,0] - 0.0, 2) + torch.pow(start_x[:,1] - 0.0, 2) >= 3**2, :]
-    #N = start_x.shape[0]
+    #N = start_x.shape[0] 
 
-    T = 10
+    T = 15
     delta_t = neural_controller.dynamics_model.dt
     num_timesteps = int(T // delta_t)
     
@@ -73,11 +73,13 @@ def plot_dubins_cbf():
     
     fig, ax = plt.subplots(1, 1)
     ax.plot((x_history[:,0,:]).squeeze().T, (x_history[:,1,:]).squeeze().T)
-    obstacle = plt.Circle((0, 0), 2, alpha=0.5, facecolor="red")
-    ax.add_patch(obstacle)
+    ax.add_patch(plt.Circle((0, 0), 2, alpha=0.5, facecolor="red"))
+    #ax.add_patch(plt.Rectangle((-2, -2), 4, 8, alpha=0.5, facecolor="red"))
     ax.plot()
     ax.set_ylabel("y")
     ax.set_xlabel("x")
+    ax.set_ylim(-4.0, 4.0)
+    ax.set_xlim(-6.0, 6.0)
     ax.set_aspect('equal')
     ax.grid(True)
 
@@ -89,6 +91,7 @@ def plot_dubins_cbf():
     ax.grid(True)
     ax.set_title("CBF")
 
+    """
     fig, ax = plt.subplots(1, 1)
     for i in range(N):
         ax.plot(np.arange(num_timesteps) * delta_t, u_history[i,:].squeeze())
@@ -104,6 +107,7 @@ def plot_dubins_cbf():
     ax.set_xlabel("Time (s)")
     ax.grid(True)
     ax.set_title("CBF Constraints")
+    """
 
     plt.show()
 
